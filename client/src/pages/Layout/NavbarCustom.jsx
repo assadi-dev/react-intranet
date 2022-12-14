8d44c91f6d376539c457859c4fca155de840ee66
Assadi Halifa <assadi.halifa@gmail.com>
2022-12-14 10:29:44 +0100

    Affichage des données sur la navbar depuis le local storage

--- client/src/pages/Layout/NavbarCustom.jsx
+++ client/src/pages/Layout/NavbarCustom.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
@@ -6,10 +6,30 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Offcanvas from "react-bootstrap/Offcanvas";
-import { LinkContainer } from "react-router-bootstrap";
 import { Link } from "react-router-dom";
+import useStorage from "../../hooks/useStorage";
+import { STORAGE_NAME } from "../../services/const";
 
 const NavbarCustom = () => {
+  const storage = useStorage(STORAGE_NAME);
+  const [userData, setUserData] = useState({
+    firstname: "",
+    lastname: "",
+    isAdmin: false,
+    photo: "",
+  });
+
+  useEffect(() => {
+    if (storage.data)
+      setUserData((prevState) => ({
+        ...prevState,
+        firstname: storage.data.firstname,
+        lastname: storage.data.lastname,
+        isAdmin: storage.data.isAdmin,
+        photo: storage.data.photo,
+      }));
+  }, [storage.data]);
+
   return (
     <>
       {["lg"].map((expand) => (
@@ -31,16 +51,18 @@ const NavbarCustom = () => {
               </Offcanvas.Header>
               <Offcanvas.Body>
                 <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Nav.Link as={Link} to="/">
-                    Ajouter un collaborateur
-                  </Nav.Link>
+                  {userData.isAdmin ? (
+                    <Nav.Link as={Link} to="/">
+                      Ajouter un collaborateur
+                    </Nav.Link>
+                  ) : null}
 
                   <Nav.Link as={Link} to="listes-collaborateurs">
                     Liste des collaborateurs
                   </Nav.Link>
 
                   <NavDropdown
-                    title="Assadi"
+                    title={`${userData.firstname} ${userData.lastname}`}
                     id={`offcanvasNavbarDropdown-expand-${expand}`}
                   >
                     <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
