8a3b9d2b8f18e08c305a6255a0221c5391496c62
Assadi Halifa <assadi.halifa@gmail.com>
2022-12-14 16:33:48 +0100

    recuperation des messages d'erreus

--- client/src/features/collaborateurs/collaborateurSlice.js
+++ client/src/features/collaborateurs/collaborateurSlice.js
@@ -1,6 +1,27 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { fetchAll } from "./fetchCollaborateur";
 
+export const fetchAllCollaborateurAsync = createAsyncThunk(
+  "collaborateurs/fetchAll",
+  async () => {
+    try {
+      const response = await fetchAll();
+      console.log("terminé !");
+      return response.data;
+    } catch (error) {
+      let message = "";
+      if (error.response) {
+        message = error.response.data.error;
+        console.log(message);
+      } else {
+        message = error.message;
+        console.log(message);
+      }
+      throw new Error(message);
+    }
+  }
+);
+
 const initialState = {
   all: [],
   random: [],
@@ -16,9 +37,30 @@ export const collaborateurSlice = createSlice({
     getAllCollaborateurs: (state, action) => {
       state.all = action.payload;
     },
+    getError: (state, action) => {
+      state.errors = action.payload;
+    },
+  },
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAllCollaborateurAsync.pending, (state) => {
+        state.status = "loading";
+        console.log("loading");
+      })
+      .addCase(fetchAllCollaborateurAsync.fulfilled, (state, action) => {
+        state.status = "completed";
+        console.log("completed !");
+        state.all = action.payload;
+      })
+      .addCase(fetchAllCollaborateurAsync.rejected, (state, action) => {
+        state.status = "rejected";
+        console.log("failed !");
+        state.errors = action.payload;
+      });
   },
 });
 
-export const { getAllCollaborateurs } = collaborateurSlice.actions;
+export const { getAllCollaborateurs, getError } = collaborateurSlice.actions;
 
 export default collaborateurSlice.reducer;
--- client/src/pages/Acceuil/index.jsx
+++ client/src/pages/Acceuil/index.jsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
-import { getAllCollaborateurs } from "../../features/collaborateurs/collaborateurSlice";
+import {
+  fetchAllCollaborateurAsync,
+  getAllCollaborateurs,
+  getError,
+} from "../../features/collaborateurs/collaborateurSlice";
 import useFetchData from "../../hooks/useFetch";
 import useStorage from "../../hooks/useStorage";
 import { STORAGE_NAME } from "../../services/const";
@@ -8,19 +12,14 @@ import { STORAGE_NAME } from "../../services/const";
 const Acceuil = () => {
   const dispatch = useDispatch();
   const storage = useStorage(STORAGE_NAME);
-  const [token, setToken] = useState("");
-  const fetchRandomUser = useFetchData("GET", "/api/collaborateurs/random", {
-    headers: {
-      "Content-Type": "application/",
-      authorization: `Bearer ${token}`,
-    },
-  });
-  if (storage.data) {
-  }
 
   useEffect(() => {
-    setToken(storage.data.token);
-  }, [fetchRandomUser.isReady, storage.data]);
+    dispatch(fetchAllCollaborateurAsync())
+      .unwrap()
+      .catch((rejectedValueOrSerializedError) => {
+        dispatch(getError(rejectedValueOrSerializedError.message));
+      });
+  }, []);
 
   return <div>Acceuil</div>;
 };
