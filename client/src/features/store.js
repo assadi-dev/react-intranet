55bc1794e0ada5d78b71ba4ab23d8f8ae22752ae
Assadi Halifa <assadi.halifa@gmail.com>
2022-12-16 13:10:54 +0100

    Update store.js

--- client/src/features/store.js
+++ client/src/features/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import collaborateurReducer from "../features/collaborateurs/collaborateurSlice";
+import ownUserSliceReducer from "../features/ownUser/ownUserSlice";
 
 export const store = configureStore({
   reducer: {
     collaborateur: collaborateurReducer,
+    ownUser: ownUserSliceReducer,
   },
 });
