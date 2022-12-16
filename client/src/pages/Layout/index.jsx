5243ec22fffbf4afaa8da38be119bc80f9d5002d
Assadi Halifa <assadi.halifa@gmail.com>
2022-12-16 13:10:52 +0100

    Update index.jsx

--- client/src/pages/Layout/index.jsx
+++ client/src/pages/Layout/index.jsx
@@ -1,16 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import NavbarCustom from "./NavbarCustom";
 
 const Layout = () => {
   return (
     <Container fluid className="layoutContainer">
       <NavbarCustom />
-
       <Outlet />
     </Container>
   );
