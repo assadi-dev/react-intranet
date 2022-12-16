7ae364cc3cbddb8efcf452dbcef6896ec042ffa2
Assadi Halifa <assadi.halifa@gmail.com>
2022-12-16 13:02:06 +0100

    Affichage de la listes collaborateurs

--- client/src/pages/ListCollaborateurs/ListCollaborateur.styled.js
+++ client/src/pages/ListCollaborateurs/ListCollaborateur.styled.js
@@ -16,23 +16,6 @@ export const ListCollaborateursCardContainer = styled.div`
   }
 `;
 
-export const RowActionsFilter = styled.div`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  .col-left {
-    display: flex;
-    justify-content: flex-start;
-  }
-
-  .col-right {
-    display: flex;
-    justify-content: flex-end;
-  }
-`;
-
-export const SearchBar = styled.div``;
-
 export const RowlistCollabContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 150px);
@@ -45,5 +28,65 @@ export const RowlistCollabContainer = styled.div`
 
   @media (min-width: 992px) {
     grid-template-columns: repeat(auto-fit, 280px);
+  }
+`;
+
+// Zone des filtre des filtres
+export const RowActionsFilter = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-rows: 20px 100px;
+  grid-gap: 8px;
+  align-items: center;
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-rows: 1fr;
+    grid-template-columns: repeat(2, 1fr);
+  }
+  .focus {
+    background-color: var(--bs-gray-400);
+    font-weight: normal;
+  }
+`;
+
+//Barre de Recherche
+export const SearchBarContainer = styled.div`
+  width: 100%;
+  border: 1px solid var(--bs-gray-600);
+  background-color: var(--bs-gray-100);
+  padding: 4px 10px;
+  border-radius: 6px;
+
+  input {
+    width: 100%;
+    height: 100%;
+    background-color: transparent !important;
+    outline: none;
+    font-weight: bold;
+    ::placeholder {
+      font-weight: normal;
+    }
+  }
+`;
+
+//Filtre Selecteur
+export const SelectFilterContainer = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+  flex-direction: column;
+  margin-bottom: 12px;
+
+  .selectInput {
+    margin-bottom: 2px;
+    @media (min-width: 992px) {
+      width: 300px;
+      margin-bottom: 0;
+    }
+  }
+
+  @media (min-width: 668px) {
+    flex-direction: row;
+    margin-bottom: 0;
+    justify-content: space-evenly;
   }
 `;
--- client/src/pages/ListCollaborateurs/index.jsx
+++ client/src/pages/ListCollaborateurs/index.jsx
@@ -1,5 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import ListCollaborateursCard from "../../components/Cards/ListCollaborateursCard";
+import { fetchAllCollaborateurAsync } from "../../features/collaborateurs/collaborateurAsyncAction";
+import {
+  allCollaborateurs,
+  errorsCollaborateurs,
+  statusCollaborateurs,
+} from "../../features/collaborateurs/collaborateurSlice";
+import FilterZone from "./FilterZone";
 import {
   ListCollaborateursCardContainer,
   RowActionsFilter,
@@ -7,13 +15,52 @@ import {
 } from "./ListCollaborateur.styled";
 
 const ListCollaborateurs = () => {
+  const dispatch = useDispatch();
+  const error = useSelector((state) => errorsCollaborateurs(state));
+  const getAllCollaborateurs = useSelector((state) => allCollaborateurs(state));
+  const getStatus = useSelector((state) => statusCollaborateurs(state));
+
+  useEffect(() => {
+    dispatch(fetchAllCollaborateurAsync())
+      .unwrap()
+      .catch((error) => dispatch(getError(error.message)));
+  }, []);
+
+  const RenderList = () => {
+    return (
+      <>
+        {getAllCollaborateurs ? (
+          getAllCollaborateurs.map((collaborateur) => (
+            <ListCollaborateursCard
+              key={collaborateur.id}
+              id={collaborateur.id}
+              name={`${collaborateur.firstname} ${collaborateur.lastname}`}
+              from={`${collaborateur.city}, ${collaborateur.country}`}
+              email={collaborateur.email}
+              phone={collaborateur.phone}
+              birthdate={collaborateur.birthdate}
+              age={`${32} ans`}
+              photo={collaborateur.photo}
+              service={collaborateur.service}
+            />
+          ))
+        ) : (
+          <div>Collaborateurs introuvable</div>
+        )}
+      </>
+    );
+  };
+
   return (
     <ListCollaborateursCardContainer>
-      <RowActionsFilter></RowActionsFilter>
+      <FilterZone />
+
       <RowlistCollabContainer>
-        <ListCollaborateursCard /> <ListCollaborateursCard />{" "}
-        <ListCollaborateursCard /> <ListCollaborateursCard />{" "}
-        <ListCollaborateursCard /> <ListCollaborateursCard />
+        {getStatus == "completed" ? (
+          <RenderList />
+        ) : (
+          <div>Chargement en cours</div>
+        )}
       </RowlistCollabContainer>
     </ListCollaborateursCardContainer>
   );
