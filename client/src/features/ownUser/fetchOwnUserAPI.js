57f2e78043336e04784c346f31961bb9d5126aee
Assadi Halifa <assadi.halifa@gmail.com>
2022-12-17 22:44:38 +0100

    refactor supression fonction inutile

--- client/src/features/ownUser/fetchOwnUserAPI.js
+++
@@ -1,9 +0,0 @@
-import { Api } from "../../services/Api";
-
-export function fetchOwnerUser(id) {
-  return Api.get(`/api/collaborateurs/${id}`);
-}
-
-export function fetchUpdateOwnerUser(id, data) {
-  return Api.put(`/api/collaborateurs/${id}`, data);
-}
